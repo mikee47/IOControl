5dbf0ca48609cb893b05593c5bb8821c1e7d1bbe
mikee47 <mike@sillyhouse.net>
2020-06-23 21:12:42 +0100

    Move `PRINT_HEAP` into Debug.h

--- src/IO/Controller.cpp
+++ src/IO/Controller.cpp
@@ -2,21 +2,7 @@
 #include <IO/Request.h>
 #include <IO/DeviceManager.h>
 #include <IO/Strings.h>
-
-#ifdef ENABLE_HEAP_PRINTING
-#ifdef ENABLE_MALLOC_COUNT
-#include <malloc_count.h>
-
-#define PRINT_HEAP()                                                                                                   \
-	debug_i("%s %s #%u : Free Heap = %u, Used = %u, Peak = %u", __FILE__, __FUNCTION__, __LINE__,                      \
-			system_get_free_heap_size(), MallocCount::getCurrent(), MallocCount::getPeak())
-#else
-#define PRINT_HEAP()                                                                                                   \
-	debug_i("%s %s #%u : Free Heap = %u", __FILE__, __FUNCTION__, __LINE__, system_get_free_heap_size())
-#endif
-#else
-#define PRINT_HEAP()
-#endif
+#include <IO/Debug.h>
 
 // Controller attempts device restart on error at this interval
 #define DEVICECHECK_INTERVAL 10000
